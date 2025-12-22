9445974701762924fc82ba1f20dd2aa37ddaa8fb
jscottvogel <j_scott_vogel@yahoo.com>
2025-12-21 19:52:26 -0600

    feat: introduce `sendHeartbeatNotification` mutation, replacing `testMutation`

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -6,9 +6,15 @@ const schema = a.schema({
         success: a.boolean().required(),
         message: a.string(),
     }),
-    testMutation: a.mutation()
-        .arguments({})
-        .returns(a.string())
+    sendHeartbeatNotification: a
+        .mutation()
+        .arguments({
+            recipientEmail: a.string().required(),
+            link: a.string().required(),
+            subject: a.string().required(),
+            messageBody: a.string().required()
+        })
+        .returns(a.ref('NotificationResponse'))
         .authorization(allow => [allow.authenticated()])
         .handler(a.handler.function(sendNotification)),
 });
