036dcb4dbd99ccb52da30b5f9f3d818edcebe401
jscottvogel <j_scott_vogel@yahoo.com>
2025-12-23 09:23:35 -0600

    Continued debugging of circular dependency in cloud formation

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,5 +1,5 @@
 import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
-// import { sendNotification } from '../functions/send-notification/resource';
+import { sendNotification } from '../functions/send-notification/resource';
 
 const schema = a.schema({
     NotificationResponse: a.customType({
@@ -153,17 +153,17 @@ const schema = a.schema({
     })
         .authorization(allow => [allow.owner(), allow.authenticated()]),
 
-    // sendHeartbeatNotification: a
-    //     .mutation()
-    //     .arguments({
-    //         recipientEmail: a.string().required(),
-    //         link: a.string().required(),
-    //         subject: a.string().required(),
-    //         messageBody: a.string().required()
-    //     })
-    //     .returns(a.ref('NotificationResponse'))
-    //     .authorization(allow => [allow.authenticated()])
-    //     .handler(a.handler.function(sendNotification)),
+    sendHeartbeatNotification: a
+        .mutation()
+        .arguments({
+            recipientEmail: a.string().required(),
+            link: a.string().required(),
+            subject: a.string().required(),
+            messageBody: a.string().required()
+        })
+        .returns(a.ref('NotificationResponse'))
+        .authorization(allow => [allow.authenticated()])
+        .handler(a.handler.function(sendNotification)),
 });
 
 export type Schema = ClientSchema<typeof schema>;
